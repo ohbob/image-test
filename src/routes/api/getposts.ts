f341314b49dba3a239ef6c2f0b3fda19ac4c943e
q <roberts@nallus.com>
2022-04-17 19:52:36 +0300

    image url

--- src/routes/api/getposts.ts
+++ src/routes/api/getposts.ts
@@ -3,7 +3,7 @@
  import type { EndpointOutput } from '@sveltejs/kit';
     
  export async function get(): Promise<EndpointOutput> {
-     const res = await fetch('http://144.91.100.171:1337/api/posts');
+     const res = await fetch('http://server.nallus.com:1337/api/posts');
      const data = await res.json();
  
      return { body: data };
